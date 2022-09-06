0ec69e96e778a2443f559fcce92ba122cf280b4d
Vdelggado <victor_delgado@live.com>
2022-09-06 09:25:12 -0500

    lab02

---
+++ lab24.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 2.4</title>
+</head>
+<body>
+    <?php
+    // creacion de arreglo array("clave"=> "valor")
+
+    $personas = array("juan"=> "Panama","john" => "USA",
+    "eica" => "Finlandia",
+    "kusanagi"=> "japon");
+    // recorrer todo el arreglo
+    foreach($personas as $persona => $pais){
+        print "$persona es de $pais <br>";
+    }
+    echo "<br>".$personas["juan"];
+    echo "<br>".$personas["eica"];
+    ?>
+</body>
+</html>
---
+++ lab27.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laboratorio 2.7</title>
+</head>
+<body>
+    <?php 
+    $posicion = "arriba";
+    switch($posicion){
+        case "arriba": 
+            echo " la variable contiene";
+            echo " el valor arriba";
+            break;
+        
+        case "abajo":
+            echo " la variable contiene";
+            echo " el valor abajo";
+            break;
+        
+        default: 
+            echo " la variable contiene otro valor";
+            echo "distinto de arriba y abajo";
+    }
+    
+    ?>
+</body>
+</html>
